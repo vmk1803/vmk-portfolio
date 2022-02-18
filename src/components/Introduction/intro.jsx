7ee0c5baee91df4edd2eb4b32a33f79915686a64
vmk1803 <vmk1803@gmail.com>
2022-02-18 14:32:55 +0530

    updated profile

--- src/components/Introduction/intro.jsx
+++ src/components/Introduction/intro.jsx
@@ -41,7 +41,7 @@ export const Intro = () => {
       <AboutDiv>
         <div id="image">
           <img
-            src="https://lh3.googleusercontent.com/whwwSuTaMh69xiiZpuuoJ2ArSvesN_YB6ibkg7aIPcrNPLeH_Pm6hM7cuS-stYGkoL5-EZ25rHX_bibIOv8k_4y6d33RbepLwPotUE2sA0VivmnnOMmV5KTsfOlZW1vSlzYbDe1nRl-9zvi03MtIi-pnVbyLtgGF1S3JUeuk6vwMeXKS5eg2gpTD-mDSo123akvrLE_w9MMhvUfoEp0ZVlfTFwqJKDjRKaRM27WhVFeOtijNWM8WR3mDFl1BTsjKn90cXdxg6K-uORSHkSlFec7jJodCa1PyUQpKI2rAl63AO3_CpnT4qKcS5XuNOsrrjd6ix2tVn8g2bpFgF66Bufpa33_hOfHM4fEM3NVM-zHeMJBip6BIbLHA1lOdmyMYpkhoAPQQv8-2Wz83-r_dwtcjorhPFStHElpINhkayY9ztnZTJLuQkYwYfdHZazeOd-ABraTT08weT1dV_LRQb8iaH7MNw8ZHCxkrlrBZyQvLrbwmdfFiL2aPATqE602FCk5g3cimZRLP6_evFtqyjsQ5F6nBMqCO7NaZylVbhdYBIodZNOuxTTT-_ygvKk9svhJoqkpeZv9Sjh9qr1pqwD6r4tnCVUyjIoZtSuRpFgaxtuIii4QdO3itnqDxa3--EIDSm-25o3vZPcd3m5LJ6ciQvS98ZWyCdB7uIsoJrrc1W7SqCS-CH2UleHCWyqLyQKOeDI5r0436gASo4e6vhyU=w804-h978-no?authuser=0"
+            src="https://i.pinimg.com/564x/7e/43/f1/7e43f10053fd1443cf41bf240e7e0225.jpg"
             alt=""
           />
         </div>
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: "https://vmk1803.netlify.app",
+  homepage: "https://muralikrishna1803.netlify.app",
   title: "VMK.",
 };
 
